f67f7fca06f7a9f5a78ebca6da6ebb7826ee551f
Vincent Conte <vconte@a-139160.ups.u-psud.fr>
2020-03-06 15:09:48 +0100

    hash functions

--- simulation.h
+++ simulation.h
@@ -4,9 +4,35 @@
 #include <map>
 #include "especemoleculaire.h"
 
+#ifndef NB_MAX_ESP
+#define NB_MAX_ESP 30
+#endif
+
+#ifndef SHIFT
+#define SHIFT 7
+#endif
+
 struct file_vars {
 	int diametre;
-	std::map<char*, EspeceMoleculaire> especes;
+	EspeceMoleculaire* especes[NB_MAX_ESP];
 };
+
+
+class EspeceMoleculaireNotFound : public std::exception {
+    private:
+        std::string esp_name;
+    public:
+        EspeceMoleculaireNotFound(char* name) { esp_name = name; }
+
+        virtual const char* what() const throw() {
+            return ("The molecular species named " + esp_name + " does not exist.").c_str();
+        }
+};
+
+
+int myhash(char*);
+EspeceMoleculaire* find(char*, EspeceMoleculaire**);
+void add(EspeceMoleculaire*, EspeceMoleculaire***);
+
 
 #endif
