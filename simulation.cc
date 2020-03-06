8e7c79e4d5d49fdffca7fac919b47c73b0410dd1
Vincent Conte <vconte@a-139160.ups.u-psud.fr>
2020-03-06 15:36:11 +0100

    hash functions definitions

--- simulation.cc
+++ simulation.cc
@@ -5,14 +5,60 @@ using namespace std;
 
 extern file_vars parse(char*);
 
+
+int myhash(char* str){
+    int result = 0;
+    for(int i = 0 ; i < strlen(str) ; i++){
+        result += str[i]*i;
+    }
+    return result%NB_MAX_ESP;
+}
+
+
+EspeceMoleculaire* find(char* name, EspeceMoleculaire** ems) {
+    int hashEM = myhash(name);
+    if(strcmp(ems[hashEM]->getNom(), name) == 0){
+        return ems[hashEM];
+    } else {
+        bool hasFound = false;
+        int hashPlusDecalage = hashEM+SHIFT;
+        while(!hasFound && hashEM != hashPlusDecalage){
+            hasFound = strcmp(ems[hashPlusDecalage]->getNom(), name) == 0;
+            hashPlusDecalage+=SHIFT;
+        }
+        if(!hasFound){
+            throw EspeceMoleculaireNotFound(name);
+        }
+        return ems[hashPlusDecalage-SHIFT];
+    }
+}
+
+
+void add(EspeceMoleculaire* emAdd, EspeceMoleculaire*** ptr_ems) {
+    int hashEM = myhash(emAdd);
+    bool hasFound = false;
+    int hashPlusDecalage = hashEM;
+    while(!hasFound && hashEM != hashPlusDecalage){
+        hasFound = ptr_ems[hashPlusDecalage] == 0;
+        if(hasFound){
+            ptr_ems[hashPlusDecalage] = emAdd;
+            hasFound = false;
+        }
+        hashPlusDecalage+=SHIFT;
+    }
+}
+
+
+
+
 int main() {
     file_vars init = parse("file.sim");
     const int diametre = init.diametre;
-	map<char*, EspeceMoleculaire> especes = init.especes;
+	EspeceMoleculaire* especes[NB_MAX_ESP] = init.especes;
 
     cout << diametre << endl;
     for (auto& x: especes) {
-        cout << x.first  << endl;
+        cout << x.getNom()  << endl;
     }
     return 0;
 }
