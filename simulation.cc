95c94b42a053d6794b84a222b9d7569acda2f6dc
Kyllian <47327666+Wyzykiki@users.noreply.github.com>
2020-03-27 16:54:32 +0100

    moved main functions to a specific file

--- simulation.cc
+++ simulation.cc
@@ -45,21 +45,3 @@ void Simulation::exportCSV() {
 
 void Simulation::run() {}
 
-
-int main() {
-    File_vars init(parse("file.sim"));
-
-    //TODO: Creer tableau de reactions et les molecules
-    cout << init.diametre << endl;
-
-
-    for (int i = 0; i<init.size; i++) {
-        cout<<init.especes[i]<<">>>"<<init.especes[i]->getNom()<<" taille: "<<init.especes[i]->getTaille()<<" vitesse: "<<init.especes[i]->getVitesse()<<" init: "<<init.especes[i]->getNbCopies()<< endl;
-        vector<Reaction*>* reactions = init.especes[i]->getReactions();
-        int nbReaction = reactions->size();
-        for (int j = 0; j<nbReaction; j++) {
-            cout << "Reac "<<j<<" : "<<*(*reactions)[j]<<endl;
-        }
-    }
-    return 0;
-}
